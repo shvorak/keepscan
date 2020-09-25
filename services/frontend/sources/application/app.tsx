5ace47a0137d317137b84b8497258e2a4e110496
Alexey Shvorak <alex@shvorak.com>
2020-09-25 12:55:02 +0300

    Main layout #WIP

--- services/frontend/sources/application/app.tsx
+++ services/frontend/sources/application/app.tsx
@@ -5,6 +5,16 @@ import { Route, Switch } from 'react-router-dom'
 export const App = () => {
     return (
         <div className={styles.layout}>
+            <div className={styles.header}>
+                <Section>
+                    <div className={styles.headline}>
+                        <div className={styles.logo}>KeepScan</div>
+                    </div>
+                </Section>
+            </div>
         </div>
     )
 }
+
+
+export const Section = ({ children }) => <div className={styles.section}>{children}</div>
