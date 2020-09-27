a815e1df626099dd4997486f058618da08ca864a
Alexey Shvorak <alex@shvorak.com>
2020-09-27 13:56:16 +0300

    Added staging environment

--- services/backend/KeepSpy.App/Controllers/DepositController.cs
+++ services/backend/KeepSpy.App/Controllers/DepositController.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using KeepSpy.App.Abstraction;
+using KeepSpy.App.Models;
 using KeepSpy.Domain;
 using KeepSpy.Storage;
 using Microsoft.AspNetCore.Mvc;
@@ -9,7 +11,7 @@ using Microsoft.EntityFrameworkCore;
 namespace KeepSpy.App.Controllers
 {
     [Route("api/[controller]")]
-    public class DepositController: BaseController
+    public class DepositController : BaseController
     {
         private readonly KeepSpyContext _db;
 
@@ -22,8 +24,33 @@ namespace KeepSpy.App.Controllers
         public Task<Deposit[]> Get() => _db.Set<Deposit>().ToArrayAsync();
 
         [HttpGet("latest")]
-        public Task<Deposit[]> Latest() 
+        public Task<Deposit[]> Latest()
             => _db.Set<Deposit>().OrderByDescending(x => x.CreatedAt).Take(10).ToArrayAsync();
 
+
+        [HttpGet("random")]
+        public async Task<RandomTdtId?> Random([FromQuery] decimal lotSize)
+        {
+            var rand = new Random();
+            var query = _db.Set<Deposit>()
+                .Where(x => x.Status == DepositStatus.Minted && x.LotSize == lotSize && x.Contract.Active);
+
+            var count = await query.CountAsync();
+
+            if (count == 0)
+            {
+                return null;
+            }
+            
+            return await query
+                .Select(x => new RandomTdtId
+                {
+                    LotSize = lotSize,
+                    Address = x.Id,
+                    ContractId = x.ContractId
+                })
+                .Skip(rand.Next(count))
+                .FirstAsync();
+        }
     }
 }
---
+++ services/backend/KeepSpy.App/Models/RandomTdtId.cs
@@ -0,0 +1,9 @@
+namespace KeepSpy.App.Models
+{
+    public class RandomTdtId
+    {
+        public decimal LotSize { get; set; }
+        public string Address { get; set; }
+        public string ContractId { get; set; }
+    }
+}
