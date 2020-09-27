13f764e50d23fd34922f8eea64191bb6d6acdce7
Alexey Shvorak <alex@shvorak.com>
2020-09-27 23:19:03 +0300

    Added sample for charts

--- services/frontend/sources/features/dashboard/cards/graph-card/index.tsx
+++ services/frontend/sources/features/dashboard/cards/graph-card/index.tsx
@@ -2,17 +2,30 @@ import React from 'react'
 import styles from './index.css'
 import { Card, CardBody, CardHead } from 'uikit/layout/card'
 import { Placeholder } from 'uikit/display/placeholder'
+import { Bar, BarChart, ResponsiveContainer, XAxis } from 'recharts'
+
+const DataMock = [
+    { date: '25 Sep', amount: 350 },
+    { date: '26 Sep', amount: 200 },
+    { date: '27 Sep', amount: 674 },
+    { date: '28 Sep', amount: 230 },
+    { date: '29 Sep', amount: 454 },
+    { date: '30 Sep', amount: 600 },
+]
 
 export const GraphCard = ({ title, children = null }) => {
-    const content = children ? children : (
-        <Placeholder wide>soon</Placeholder>
-    )
+    const content = children ? children : <Placeholder wide>soon</Placeholder>
 
     return (
         <Card className={styles.card}>
             <CardHead stroked={false}>{title}</CardHead>
             <CardBody className={styles.body}>
-                {content}
+                <ResponsiveContainer width="100%" height={135}>
+                    <BarChart width={200} height={200} data={DataMock}>
+                        <Bar dataKey="amount" fill="#7850cd" />
+                        <XAxis dataKey="date" />
+                    </BarChart>
+                </ResponsiveContainer>
             </CardBody>
         </Card>
     )
