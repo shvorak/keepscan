42c5baf3b6f4253e0586c63a86170dc2066b155e
Alexey Shvorak <alex@shvorak.com>
2020-09-28 01:02:11 +0300

    Added mock data factory and increased days count

--- services/frontend/sources/features/dashboard/cards/graph-card/index.tsx
+++ services/frontend/sources/features/dashboard/cards/graph-card/index.tsx
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import styles from './index.css'
 import { Card, CardBody, CardHead } from 'uikit/layout/card'
 import { Placeholder } from 'uikit/display/placeholder'
-import { Bar, BarChart, ResponsiveContainer, XAxis } from 'recharts'
+import { Bar, BarChart, ResponsiveContainer, Tooltip, XAxis } from 'recharts'
+import { format, subDays } from 'date-fns'
 
-const DataMock = [
-    { date: '25 Sep', amount: 350 },
-    { date: '26 Sep', amount: 200 },
-    { date: '27 Sep', amount: 674 },
-    { date: '28 Sep', amount: 230 },
-    { date: '29 Sep', amount: 454 },
-    { date: '30 Sep', amount: 600 },
-]
+const generateData = () => {
+    const now = Date.now()
+
+    return new Array(10).fill(null).map((_, i) => {
+        return {
+            date: format(subDays(now, 10 - i), 'dd MMM'),
+            amount: Math.floor(Math.random() * 600) + 200
+        }
+    })
+}
+
 
 export const GraphCard = ({ title, children = null }) => {
     const content = children ? children : <Placeholder wide>soon</Placeholder>
-
+    const data = useMemo(() => generateData(), [])
     return (
         <Card className={styles.card}>
             <CardHead stroked={false}>{title}</CardHead>
             <CardBody className={styles.body}>
                 <ResponsiveContainer width="100%" height={135}>
-                    <BarChart width={200} height={200} data={DataMock}>
+                    <BarChart width={200} height={200} data={data}>
+                        <Tooltip />
                         <Bar dataKey="amount" fill="#7850cd" />
                         <XAxis dataKey="date" />
                     </BarChart>
