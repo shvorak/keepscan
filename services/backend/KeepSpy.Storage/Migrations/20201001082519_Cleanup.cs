5b95646806208190dfa19566d60130a040430c1b
Alexey Shvorak <alex@shvorak.com>
2020-10-01 11:28:11 +0300

    cleanup migration

---
+++ services/backend/KeepSpy.Storage/Migrations/20201001082519_Cleanup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KeepSpy.Storage.Migrations
+{
+    public partial class Cleanup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"delete from transaction");
+            migrationBuilder.Sql(@"delete from redeem");
+            migrationBuilder.Sql(@"delete from deposit");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
