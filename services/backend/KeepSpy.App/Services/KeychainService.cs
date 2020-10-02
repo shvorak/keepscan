c26ccd82089d43906fb882f9e5209549b484c6d8
Alexey Shvorak <alex@shvorak.com>
2020-10-02 16:27:45 +0300

    Fix keychain client

--- services/backend/KeepSpy.App/Services/KeychainService.cs
+++ services/backend/KeepSpy.App/Services/KeychainService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 
@@ -12,10 +13,17 @@ namespace KeepSpy.App.Services
             _http = http;
         }
 
-        public Task<string> GetBtcAddress(string keyX, string keyY, bool isTest = true)
+        public async Task<string?> GetBtcAddress(string keyX, string keyY, bool isTest = true)
         {
             var network = isTest ? "testnet" : "mainnet";
-            return _http.GetStringAsync($"/btc/pubkey?x={keyX}&y={keyY}&network={network}");
+            try
+            {
+                return await _http.GetStringAsync($"/btc/pubkey?x={keyX}&y={keyY}&network={network}");
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
         }
         
     }
