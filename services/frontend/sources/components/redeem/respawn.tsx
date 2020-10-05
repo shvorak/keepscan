cca1d012f9112cee4288c71108e4b941dd111cb7
Alexey Shvorak <alex@shvorak.com>
2020-10-05 15:34:12 +0300

    Fixed respawn link

--- services/frontend/sources/components/redeem/respawn.tsx
+++ services/frontend/sources/components/redeem/respawn.tsx
@@ -7,7 +7,7 @@ import { DAPP } from '~/application/env'
 const RespawnActions = {
     [RedeemStatus.Requested]: 'deposit/{0}/redemption',
     [RedeemStatus.Signed]: 'deposit/{0}/redemption/signing',
-    [RedeemStatus.BtcTransferred]: 'redeem/{0}/redemption/confirming',
+    [RedeemStatus.BtcTransferred]: 'deposit/{0}/redemption/confirming',
 }
 
 export const RedeemRespawn = ({ status, redeem }) => {
