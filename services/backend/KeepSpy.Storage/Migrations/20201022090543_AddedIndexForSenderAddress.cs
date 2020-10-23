a8186897961ab954ecb69dc76d22b5db8b4c4115
Alexey Shvorak <alex@shvorak.com>
2020-10-23 19:43:12 +0300

    Fixed migration failure

--- services/backend/KeepSpy.Storage/Migrations/20201022090543_AddedIndexForSenderAddress.cs
+++ services/backend/KeepSpy.Storage/Migrations/20201022090543_AddedIndexForSenderAddress.cs
@@ -6,26 +6,26 @@ namespace KeepSpy.Storage.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateIndex(
-                name: "ix_redeem_sender_address",
-                table: "redeem",
-                column: "sender_address");
-
-            migrationBuilder.CreateIndex(
-                name: "ix_deposit_sender_address",
-                table: "deposit",
-                column: "sender_address");
+            // migrationBuilder.CreateIndex(
+            //     name: "ix_redeem_sender_address",
+            //     table: "redeem",
+            //     column: "sender_address");
+            //
+            // migrationBuilder.CreateIndex(
+            //     name: "ix_deposit_sender_address",
+            //     table: "deposit",
+            //     column: "sender_address");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropIndex(
-                name: "ix_redeem_sender_address",
-                table: "redeem");
-
-            migrationBuilder.DropIndex(
-                name: "ix_deposit_sender_address",
-                table: "deposit");
+            // migrationBuilder.DropIndex(
+            //     name: "ix_redeem_sender_address",
+            //     table: "redeem");
+            //
+            // migrationBuilder.DropIndex(
+            //     name: "ix_deposit_sender_address",
+            //     table: "deposit");
         }
     }
 }
