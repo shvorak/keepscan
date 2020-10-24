8beb5777f71e8d7805bc8f30ebbdcbe16f35cc62
Alexey Shvorak <alex@shvorak.com>
2020-10-24 12:26:12 +0300

    Changed deploy config

--- services/backend/KeepSpy.Domain/RedeemStatus.cs
+++ services/backend/KeepSpy.Domain/RedeemStatus.cs
@@ -2,12 +2,12 @@
 {
 	public enum RedeemStatus
 	{
-		Requested = 0,
-		Signed = 1,
-		Redeemed = 2,
-		Liquidation = 3,
-		Liquidated = 4,
-		BtcTransferred = 5,
-		OperationFailed = 6
+		Requested,
+		Signed,
+		Redeemed,
+		Liquidation,
+		Liquidated,
+		BtcTransferred,
+		OperationFailed
 	}
 }
