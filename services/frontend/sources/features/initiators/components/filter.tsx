41adb8802d990c31fb2c3ee680189f05fc442d66
Alexey Shvorak <alex@shvorak.com>
2020-11-04 11:03:58 +0300

    Fixed initiators page

--- services/frontend/sources/features/initiators/components/filter.tsx
+++ services/frontend/sources/features/initiators/components/filter.tsx
@@ -1,5 +1,4 @@
 import React, { FC, useEffect } from 'react'
-import styles from './filter.less'
 import { Query } from 'shared/types'
 import { Search } from 'uikit/control'
 import { useModel } from 'shared/hooks/controls'
@@ -17,7 +16,7 @@ export const InitiatorFilter: FC<InitiatorFilterProps> = ({ query, onChange, ...
     }, [values, onChange])
 
     return (
-        <div className={styles.root} {...props}>
+        <div {...props}>
             <Search label="Search by address" value={values.search} onChange={onChangeField('search')} minLength={4} />
         </div>
     )
