b551684543ed51fe0fd6adb7001acf405324c34f
Максим Федоров <makstl@mail.ru>
2020-11-11 21:42:37 +0300

    Update last processed block

---
+++ services/backend/KeepSpy.Storage/Migrations/20201111183402_UpdateLastProcessed.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KeepSpy.Storage.Migrations
+{
+    public partial class UpdateLastProcessed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update network set last_block_processed = 11187370 where Kind = 1 and Is_testnet = false");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
